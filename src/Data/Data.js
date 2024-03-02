d3f82d4cac113675bb9ab9dc41a3ce898a158a8c
ZRNOF <ZRNOF@protonmail.com>
2024-03-03 03:21:29 +0800

    feat: integrate Data functions into Olon class

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,13 +1,16 @@
+import Olon from "../Core/main.js"
 import * as TypeMaps from "./TypeMaps.js"
-
-export const DataType = (data) => data.constructor.name
 
 const flat = (array) => array.flat(Infinity)
 
-const Data = (data, type = "f32") => {
+Olon.prototype.DataType = function (data) {
+	return data.constructor.name
+}
+
+Olon.prototype.Data = function (data, type = "f32") {
 	if (Array.isArray(data)) return new TypeMaps.Data[type](flat(data))
 	if (typeof data === "number") return new TypeMaps.Data[type](data)
 	throw new Error("Unsupported data type")
 }
 
-export default Data
+export default Olon
