2fbb03eb42e62496c11e273f9118d8b73531fe65
ZRNOF <ZRNOF@protonmail.com>
2024-03-10 22:04:12 +0800

    feat: enable texture2D() to create an empty texture

--- src/Texture/texture.js
+++ src/Texture/texture.js
@@ -4,9 +4,9 @@ Olon.prototype._isDOMElement = function (texture) {
 	return texture instanceof HTMLElement ? true : false
 }
 
-Olon.prototype.setTexParameter = function (wrapX, wrapY, minFilter, magFilter) {
-	this.gl.texParameteri(this.T2D, this.gl.TEXTURE_WRAP_S, wrapX)
-	this.gl.texParameteri(this.T2D, this.gl.TEXTURE_WRAP_T, wrapY)
+Olon.prototype.setTexParameter = function (wrapS, wrapT, minFilter, magFilter) {
+	this.gl.texParameteri(this.T2D, this.gl.TEXTURE_WRAP_S, wrapS)
+	this.gl.texParameteri(this.T2D, this.gl.TEXTURE_WRAP_T, wrapT)
 	this.gl.texParameteri(this.T2D, this.gl.TEXTURE_MIN_FILTER, minFilter)
 	this.gl.texParameteri(this.T2D, this.gl.TEXTURE_MAG_FILTER, magFilter)
 }
@@ -25,70 +25,95 @@ Olon.prototype.useTexture2D = function (texture, callback) {
 	this.unbindTexture2D()
 }
 
-Olon.prototype.emptyTexture2D = function (
-	width,
-	height,
-	iformat,
-	{
-		minFilter = this.NML,
-		magFilter = this.LINEAR,
-		filter = undefined,
-		wrapX = this.REPEAT,
-		wrapY = this.REPEAT,
-		wrap = undefined,
-		flipY = false,
-	} = {}
-) {
+Olon.prototype._emptyTexStorage2D = function ({
+	width = this.width,
+	height = this.height,
+	iformat = undefined,
+	minFilter = this.LINEAR,
+	magFilter = this.LINEAR,
+	filter = undefined,
+	wrapS = this.REPEAT,
+	wrapT = this.REPEAT,
+	wrap = undefined,
+	flipY = false,
+} = {}) {
+	if (!iformat)
+		throw "_emptyTexStorage2D: Please specify iformat (internalformat)."
 	if (filter) [minFilter, magFilter] = [filter, filter]
-	if (wrap) [wrapX, wrapY] = [wrap, wrap]
+	if (wrap) [wrapS, wrapT] = [wrap, wrap]
 	const texture = this.gl.createTexture()
 	this.useTexture2D(texture, () => {
 		this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, flipY)
-		this.setTexParameter(wrapX, wrapY, minFilter, magFilter)
+		this.setTexParameter(wrapS, wrapT, minFilter, magFilter)
 		this.gl.texStorage2D(this.T2D, 1, iformat, width, height)
 	})
 	return texture
 }
 
-Olon.prototype.texture2D = function (
-	{
-		data = null,
-		iformat,
-		level = 0,
-		width,
-		height,
-		minFilter = this.NML,
-		magFilter = this.LINEAR,
-		filter = undefined,
-		wrapX = this.REPEAT,
-		wrapY = this.REPEAT,
-		wrap = undefined,
-		flipY = this._isDOMElement(data),
-	} = {
-		data,
-		iformat,
-		level,
-		width,
-		height,
-		minFilter,
-		magFilter,
-		filter,
-		wrapX,
-		wrapY,
-		wrap,
-		flipY,
-	}
-) {
-	if (!width) width = data?.width
-	if (!height) height = data?.height
-	if (!width) throw "Please specify width."
-	if (!height) throw "Please specify height."
+Olon.prototype._emptyTexImage2D = function ({
+	width = this.width,
+	height = this.height,
+	iformat = undefined,
+	minFilter = this.LINEAR,
+	magFilter = this.LINEAR,
+	filter = undefined,
+	wrapS = this.REPEAT,
+	wrapT = this.REPEAT,
+	wrap = undefined,
+	flipY = false,
+} = {}) {
+	if (!iformat)
+		throw "_emptyTexImage2D: Please specify iformat (internalformat)."
 	if (filter) [minFilter, magFilter] = [filter, filter]
-	if (wrap) [wrapX, wrapY] = [wrap, wrap]
+	if (wrap) [wrapS, wrapT] = [wrap, wrap]
+	const { format, type } = this.IFormatMap[iformat]
+	const texture = this.gl.createTexture()
+	this.useTexture2D(texture, () => {
+		this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, flipY)
+		this.setTexParameter(wrapS, wrapT, minFilter, magFilter)
+		this.gl.texImage2D(
+			this.T2D,
+			0,
+			iformat,
+			width,
+			height,
+			0,
+			format,
+			type,
+			null
+		)
+	})
+	return texture
+}
+
+Olon.prototype.texture2D = function ({
+	data = null,
+	iformat = undefined,
+	level = 0,
+	width = data?.width || this.width,
+	height = data?.height || this.height,
+	minFilter = this.LINEAR,
+	magFilter = this.LINEAR,
+	filter = undefined,
+	wrapS = this.REPEAT,
+	wrapT = this.REPEAT,
+	wrap = undefined,
+	flipY = this._isDOMElement(data),
+	immutableStorage = false,
+} = {}) {
+	if (!iformat) throw "texture2D: Please specify iformat (internalformat)."
 
 	// create empty texture
-	// prettier-ignore
-	if (!data) return this.emptyTexture2D(width, height, { minFilter, magFilter, wrapX, wrapY })
+	if (!data) {
+		// prettier-ignore
+		const options = { width, height, iformat, minFilter, magFilter, filter, wrapS, wrapT, wrap, flipY }
+		return immutableStorage
+			? this._emptyTexStorage2D(options)
+			: this._emptyTexImage2D(options)
+	}
+
+	if (filter) [minFilter, magFilter] = [filter, filter]
+	if (wrap) [wrapS, wrapT] = [wrap, wrap]
 
 	const { format, type } = this.IFormatMap[iformat]
 	const texture = this.gl.createTexture()
@@ -106,7 +131,7 @@ Olon.prototype.texture2D = function (
 		data
 	)
 	this.gl.generateMipmap(this.T2D)
-	this.setTexParameter(wrapX, wrapY, minFilter, magFilter)
+	this.setTexParameter(wrapS, wrapT, minFilter, magFilter)
 	this.gl.bindTexture(this.T2D, null)
 	return texture
 }
