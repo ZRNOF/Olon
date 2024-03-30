87863d7011a9ce4c9f55e0503875b7689fe20df8
ZRNOF <ZRNOF@protonmail.com>
2024-03-31 00:05:53 +0800

    feat: add quadBufferInfo()

--- src/Data/vertices.js
+++ src/Data/vertices.js
@@ -11,6 +11,22 @@ Olon.prototype.quadData = function () {
 	])
 }
 
+/////////////////////////////////////////////
+// [*] REFACTOR /////////////////////////////
+Olon.prototype.quadBufferInfo = function (
+	positionName = "aPosition",
+	texCoordName = "aTexCoord"
+) {
+	return {
+		buffer: this.createBuffer(this.quadData(), this.STATIC_DRAW),
+		stride: 4 * 4,
+		attributes: [
+			{ name: positionName, unit: "f32", size: 2 },
+			{ name: texCoordName, unit: "f32", size: 2 },
+		],
+	}
+}
+
 Olon.prototype.sketchData = function () {
 	const positionData = this.Data([-1, -1, 1, -1, 1, 1, 1, 1, -1, 1, -1, -1])
 	const texCoordData = this.Data([0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0])
